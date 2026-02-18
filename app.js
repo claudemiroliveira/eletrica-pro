fcae1605e911f3cb1181c1305360b1e068e54ead
claudemiroliveira <131096494+claudemiroliveira@users.noreply.github.com>
2026-02-18 02:13:55 -0300

    Refactor abrirTela and calcular functions

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ tela.innerHTML=`
 <p>✔ Cálculos rápidos</p>
 <p>✔ Tabelas elétricas</p>
 
-</div>;
+</div>`;
 }
 
 /* TABELAS */
@@ -117,6 +117,7 @@ document.getElementById("resultado").innerHTML =
 }
 
 abrirTela("inicio");
+
 
 
 
